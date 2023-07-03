857774c84d7ef5c827601baf68b6a829065b1085
Ibrahimdata1 <bsk.kosit1@gmail.com>
2023-07-03 12:17:52 +0700

    error Javascript
    confused how to remove()

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-function clicktoAdd(){
-    let result = document.getElementById('box-input-text').value
-    let showResult = document.createElement('h2')
-    showResult.innerHTML = result
-    document.getElementById('list').appendChild(showResult)
+const inputBox = document.getElementById('texthere')
+const listContainer = document.getElementById('list-container')
+
+function addTask(){
+    if (inputBox.value === ''){
+        alert('Write SOmethings!')
+    }else{
+        let newElement = document.createElement('li')
+        newElement.innerHTML = inputBox.value
+        listContainer.appendChild(newElement)
+        let del = document.createElement('span')
+        del.innerHTML = '\u00d7'
+        newElement.appendChild(del)
+    }
+    inputBox.value = ''
 }
+listContainer.addEventListener('click',function(event){
+    if (event.target.tagName === 'span'){
+        e.target.parentELement.remove()
+    }
+})
